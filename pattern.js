992792a36b3062df2962a1f65f653d12b9b2fc48
AkashHalder520 <akashhalder520@gmail.com>
2023-08-01 20:20:05 +0530

    one pattern completed

--- pattern.js
+++ pattern.js
@@ -11,7 +11,7 @@ in this case we can divide tha pattern into 3 parts
 
 for the first part
 
-let i be the number of lines and j be a random value here 10 jend will be no.of stars -1 +initial j
+let i be the number of lines and j be a random value here 10 jend will be no.of stars -1 + j
 
 diff = -1 as its decreasing by one
 1 ***** 10  14  
@@ -35,19 +35,38 @@ for (var i = 1; i <=5 ; i++) {
     str=str+"*";
     
    }
-
+//2nd loop
+for (var j = 10; j <=(2*i)+7 ; j++) {
+    
+   str=str+"1";// using 1 for the spaces 
+   
+  }
+  for (var j = 10; j <=15-i ; j++) { // 3rd iteration will be same as first one 
+    
+   str=str+"*";
+   
+  }
    console.log(str);
     
 }
 
 /*
 
-**********
-****11****
-***1111***
-**111111**
-*11111111*
+**********  10  9
+****11****  10  11
+***1111***  10  13 
+**111111**  10  15
+*11111111*  10  17
 
 we denote the space by 1
+for space we have to take another loop 
+for that again we have to calculate for space
 
+let i be the number of lines and j be a random value here 10 jend will be no.of space -1 + j
+
+diff=+2
+
+jend=diff*i+j
+9=2i+c => c=7
+jend= 2i+7
 */
