c53ee868e1915f141ed659a24b872bb397886bca
AkashHalder520 <akashhalder520@gmail.com>
2024-02-06 19:42:36 +0530

    revicing promise

---
+++ promise.js
@@ -0,0 +1,27 @@
+//promise is a object that represent the completion or failure of a asyncronous operation
+const promiseOne=new Promise(function(resolve, rejected){
+// mainly to do some async task
+setTimeout(function(){
+    console.log('async ');
+    resolve(12)
+},2000)
+
+})
+
+promiseOne.then(function(xx){
+    console.log(`promise consumed${xx}`);
+})
+
+// another example
+
+new Promise(function(resolve, regect){
+    setTimeout(function(){
+        console.log('async ');
+        resolve(12)
+    },2000)
+    
+}).then(function(){
+    console.log(`promise consumed`);
+})
+
+
