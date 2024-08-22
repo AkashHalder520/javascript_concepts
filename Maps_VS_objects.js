65921423b0e9ad523646eae2ef13e02796cca0c7
AkashHalder520 <akashhalder520@gmail.com>
2024-08-22 19:54:17 +0530

    sets and map

---
+++ Maps_VS_objects.js
@@ -0,0 +1,7 @@
+const personObject={
+    name: "dom",
+    age:35,
+    occpation:"software Developer"
+}
+
+const personMap= new Map(["name", "dom"])
---
+++ sets.js
@@ -0,0 +1,7 @@
+const arr=[10,30.40,10,50]
+console.log(arr);//[ 10, 30.4, 10, 50 ]
+
+const s=new Set([10,20,10,30,50])
+console.log(s);//Set(4) { 10, 20, 30, 50 }
+// set removes dupliccate value
+// set have unique values
