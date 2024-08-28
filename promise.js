51de815cf63d2fa280e11a6cfc89fad47c21cec4
AkashHalder520 <akashhalder520@gmail.com>
2024-08-28 20:04:27 +0530

    promise update

--- promise.js
+++ promise.js
@@ -1,27 +1,48 @@
 //promise is a object that represent the completion or failure of a asyncronous operation
-const promiseOne=new Promise(function(resolve, rejected){
-// mainly to do some async task
-setTimeout(function(){
-    console.log('async ');
-    resolve(12)
-},2000)
+// const promiseOne=new Promise(function(resolve, rejected){
+// // mainly to do some async task
+// setTimeout(function(){
+//     console.log('async ');
+//     resolve(12)
+// },2000)
 
-})
+// })
 
-promiseOne.then(function(xx){
-    console.log(`promise consumed${xx}`);
-})
+// promiseOne.then(function(xx){
+//     console.log(`promise consumed${xx}`);
+// })
+
+// // another example
+
+// new Promise(function(resolve, regect){
+//     setTimeout(function(){
+//         console.log('async ');
+//         resolve(12)
+//     },2000)
+    
+// }).then(function(){
+//     console.log(`promise consumed`);
+// })
 
 // another example
-
-new Promise(function(resolve, regect){
-    setTimeout(function(){
-        console.log('async ');
-        resolve(12)
-    },2000)
+function loadingData(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log('loading data');
+            resolve()
+        },4000)
+        
+    })
+}
+function viewingData(){
+    console.log('viewing data');
     
-}).then(function(){
-    console.log(`promise consumed`);
-})
+}
+function updatingData(){
+    console.log('updaing data');
+    
+}
 
+loadingData().then(viewingData) // we should not call direct the function viewingData() 
 
+// updatingData()
