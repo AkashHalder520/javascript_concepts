81f740709d899680d42dd241f22ceaa0b029a14b
AkashHalder520 <akashhalder520@gmail.com>
2025-04-02 19:09:54 +0530

    small improvement

--- DSA_Udemy/lesson2MultiplePointer.js
+++ DSA_Udemy/lesson2MultiplePointer.js
@@ -24,3 +24,44 @@ for(let i=0;i<arr.length;i++){
 return arrRes.slice(0,2)// only for the first occurance
 }
 console.log(sumZero([-3,-2,-1,0,1,2,3]));
+
+//!optimised approach
+// basically we will use two pointer approach
+// one pointer will be at start and other at end
+// if the sum is greater than 0 then we will move the end pointer to left
+// if the sum is less than 0 then we will move the start pointer to right
+// if the sum is equal to 0 then we will return the pair    
+
+
+function sumZeroOptimised(arr){
+    let left=0;
+    let right=arr.length-1;
+    while(left<right){
+        let sum=arr[left]+arr[right]
+        if(sum===0){
+            return [arr[left],arr[right]]
+        }else if(sum>0){
+            right--
+        }else{
+            left++
+        }
+    }
+    return undefined
+}
+console.log(sumZeroOptimised([-3,-2,-1,0,1,2,3]));     
+
+//another problem ... count unique values
+/*
+Implement a function called countUniqueValues which accepts 
+example countUniqueValues([1,1,1,1,1,2]) // 2
+countUniqueValues([]) // 0
+countUniqueValues([-2,-1,-1,0,1]) // 4
+countUniqueValues([1,2,3,4,5]) // 5
+*/
+function countUniqueValues(arr){
+let pointer1=0;
+let pointer2=pointer1+1;
+while(pointer1<arr.length){
+    
+}
+}
